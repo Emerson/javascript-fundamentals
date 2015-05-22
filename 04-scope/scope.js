afd78f72212ae00821f1789f5f4dc00539ac8f4a
Derek Watson <derek@twg.ca>
2015-05-22 15:14:05 -0400

    expanded variable scope explanation

--- 04-scope/scope.js
+++ 04-scope/scope.js
@@ -1,40 +1,71 @@
 /*
 *   SCOPE
 *   =====
-*   What variables are currently available?
+*   JavaScript has weird variable scoping rules compared to other languages!
+*
+*
+*   1) Variables are scoped to their enclosing function (not to the block)
+*   2) Variables outside of any function are bound to the root scope (e.g. Window).
 */
-
 
 
 /*
-*   In the browser, the `window` object is our main global. Variables
-*   defined in the root scope are attached to the window.
+*   Variables are scope to their enclosing function
 */
 
-// console.log(window)
-
-var MyGlobal = 'hello!';
-
-// console.log(window.MyGlobal);
-
-
-
-function doSomething() {
-  console.log(MyGlobal);
+function someFunction() {
+  var name = 'Susan';
 }
 
+console.log(name);                // ReferenceError: name is not defined
+
 
 /*
-*   Variables defined in a function scope are only visible within
-*   the function and any child scopes
+*   Variables are not isolated to their block like in other languages
 */
+function myName() {
+
+  if (false) {
+    var name = 'Susan';
+  }
+  else {
+    var name = 'Fred';
+  }
+
+  console.log(name);
+}
+
+myName();                         // Fred
+
+
+/*
+*   Variables are available to all nested child scopes
+*/
+
+var day = 'Monday'
 
 function myParent() {
   var name = 'Susan';
 
   function myChild() {
-    console.log(name);
+    console.log(day, name);
   }
 
   myChild();
+}
+
+myParent();                     // Monday Susan
+
+
+/*
+*   In the browser, the `window` object is our root scope. Variables
+*   defined outside of any function are attached to the window.
+*/
+
+var myGlobal = 'hello!';
+
+console.log(window.myGlobal);   // hello!
+
+function doSomething() {
+  console.log(myGlobal);        // hello!
 }
